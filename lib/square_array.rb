5df80a689eebaa6410e8a21e701a9da7e9c1ff3e
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-07 20:35:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,4 +1,10 @@
 def square_array(array)
   new_array = []
-  while array.length 
+  count = 0
+  
+  while count < array.length
+    new_array << array[count] ** array[count]
+    count += 1
+  end
+  puts new_array
 end
